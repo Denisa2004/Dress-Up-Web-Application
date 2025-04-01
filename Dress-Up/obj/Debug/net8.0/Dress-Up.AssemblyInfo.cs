45356611f066bab6fd8c05545cfbad884a2ccf11
iriinna <irina-maria.istrate@s.unibuc.ro>
2025-04-01 08:57:32 +0300

    added gitignore

--- Dress-Up/obj/Debug/net8.0/Dress-Up.AssemblyInfo.cs
+++ Dress-Up/obj/Debug/net8.0/Dress-Up.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dress-Up")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36cb760d129bab93b3e4edc8db729efb72566d64")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dress-Up")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dress-Up")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
