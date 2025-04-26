2ad7b1641ec3010d1555ad1fcdaa2274f3bc4f9b
iriinna <irina-maria.istrate@s.unibuc.ro>
2025-04-26 15:50:06 +0300

    create models

---
+++ Dress-Up/Models/Accessory.cs
@@ -0,0 +1,13 @@
+﻿namespace Dress_Up.Models
+{
+    public class Accessory
+    {
+        public int Id { get; set; }
+        public string Category { get; set; }
+        public string Gender { get; set; }
+        public int Price { get; set; }
+        public int Rarity { get; set; }
+        public DateTime Date_added { get; set; } = DateTime.Now;
+        public string Image { get; set; }
+    }
+}
---
+++ Dress-Up/Models/Event.cs
@@ -0,0 +1,15 @@
+﻿namespace Dress_Up.Models
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Type { get; set; }
+        public DateTime Date { get; set; }
+
+        public int Duration { get; set; }
+        public ICollection <User> Users { get; set; }
+
+    }
+}
